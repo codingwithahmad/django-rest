009b994be1fe9a45da84abffab305857725dafa1
codingwithahmad <amirahmad.seraji1996@gmail.com>
2021-08-06 20:52:44 +0430

    fix security item

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w*m+#s0m2c@&&3j9ktzu@62a8#csnxwp_v#3k73$46l0tc(uxh'
+from . import config
+
+SECRET_KEY = config.DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
